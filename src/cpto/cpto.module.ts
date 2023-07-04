73ed48fcda39de7650074a1c4fcabd59a642441b
Tobias Mesquita <tobias.mesquita@gmail.com>
2023-07-04 10:15:41 -0300

    remove dead code

--- src/cpto/cpto.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CptoService } from './cpto.service';
-import { CptoController } from './cpto.controller';
-
-@Module({
-  controllers: [CptoController],
-  providers: [CptoService]
-})
-export class CptoModule {}
--- src/cpto/dto/update-cpto.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateCptoDto } from './create-cpto.dto';
-
-export class UpdateCptoDto extends PartialType(CreateCptoDto) {}
